2dbf240fbf23c7d3748864265c377b3b9d31ae3b
Partha Kashyap <parthakashyal@gmail.com>
2024-01-23 22:56:22 +0530

    Added about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12" style={{ backgroundImage: "url('https://wallpapercave.com/wp/wp5121853.png')", backgroundSize: 'cover' }}>
       <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20 bg-clip-padding bg-opacity-60 border border-gray-200" style={{ backdropFilter: 'blur(5px)' }}>
+        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20 bg-clip-padding bg-opacity-60 border border-gray-200" style={{ backdropFilter: 'blur(0px)' }}>
           <div className="max-w-md mx-auto">
             <div>
               <p className='font-bold text-3xl blue-gradient_text'>I'm Partha Pratim Kashyap</p>

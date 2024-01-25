6f97adadb4ba61ae5d65d074a47388a4c2ae1393
Partha Kashyap <parthakashyal@gmail.com>
2024-01-25 11:52:52 +0530

    All responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,27 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <header className="header">
-        <NavLink to="/" className="w-20 h-9 rounded-tl-lg rounded-br-lg bg-white items-center justify-center flex font-bold shadow-lg">
-            <p className="blue-gradient_text">Partha</p>
-
+      <NavLink to="/" className="w-20 h-9 rounded-tl-lg rounded-br-lg bg-white items-center justify-center flex font-bold shadow-lg">
+        <p className="blue-gradient_text ">Partha</p>
+      </NavLink>
+      <nav className="flex text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-xl gap-3 sm:gap-4 md:gap-6 lg:gap-8 xl:gap-10 font-medium">
+        <NavLink to="https://www.feedsreal.pro" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black' }>
+          Blog
         </NavLink>
-        <nav className="flex text-lg gap-7 font-medium">
-            <NavLink to="https://www.feedsreal.pro" className={({isActive}) => isActive ? 'text-blue-500' : 'text-black' }>
-                Blog
-            </NavLink>
-            <NavLink to="/about" className={({isActive}) => isActive ? 'text-blue-500' : 'text-black' }>
-                About
-            </NavLink>
-            <NavLink to="/projects" className={({isActive}) => isActive ? 'text-blue-500' : 'text-black' }>
-                Projects
-            </NavLink>
-            <NavLink to="/Contact" className={({isActive}) => isActive ? 'text-blue-500' : 'text-black' }>
-                Contact
-            </NavLink>
-        </nav>
+        <NavLink to="/about" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black' }>
+          About
+        </NavLink>
+        <NavLink to="/projects" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black' }>
+          Projects
+        </NavLink>
+        <NavLink to="/Contact" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black' }>
+          Contact
+        </NavLink>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
